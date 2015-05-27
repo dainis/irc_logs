86be10b85e7ae5cd7265bc7266e348a60ad61e6e
Dainis <dainis.tillers@gmail.com>
2015-05-27 09:16:18 +0300

    Added - autolink possible links

--- web_app/src/components/Message.js
+++ web_app/src/components/Message.js
@@ -1,11 +1,15 @@
 'use strict';
 
 var React = require('react/addons'),
+    ReactAutolink = require('react-autolink'),
     moment = require('moment');
 
 require('styles/Message.less');
 
 var Message = React.createClass({
+    mixins : [
+        ReactAutolink
+    ],
     render: function () {
       	var message = this.props.message,
       		d = moment(message.date).format('MMMM Do YYYY, HH:mm:ss');
@@ -14,7 +18,7 @@ var Message = React.createClass({
             <div className="Message">
                 <span className="Header">{d} | {message.from}</span>
                 <span className="Seperator"> : </span>
-                <span className="Body">{message.message}</span>
+                <span className="Body">{this.autolink(message.message, {rel: 'nofollow', target : '_blank'})}</span>
             </div>
         );
     }
